fe57c2f539f1b9b41e8164afd08c25a9d87ca244
Paulo Santos <paulohfsantos1@gmail.com>
2021-12-02 03:32:27 -0300

    set mapGetters

--- pages/index.vue
+++ pages/index.vue
@@ -2,22 +2,36 @@
   <main>
     <section class="home">
       <Header />
+      <div class="content">
+        <div
+          v-for="(movie, index) in getMovies.results"
+          :key="index"
+          class="movie__grid"
+        >
+          <span>{{ movie.title }}</span>
+        </div>
+      </div>
     </section>
   </main>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'Home',
+
+  computed: {
+    ...mapGetters(['getMovies']),
+  },
 
   mounted() {
     Promise.all([
       this.$store.commit('resetMovies'),
       this.$store.dispatch('getMovies'),
     ])
-  }
+  },
 }
 </script>
 
-<style>
-</style>
+<style></style>
